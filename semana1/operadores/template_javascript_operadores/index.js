065daa4c2b95ad6d10c9e610d8901b80a1116a1a
Muri-lo <muriloss2019@hotmail.com>
2021-06-15 16:37:36 -0300

    Operadores, exercicios

---
+++ semana1/operadores/template_javascript_operadores/index.js
@@ -0,0 +1,21 @@
+// Exercicio1: o resultado é false, false, True.
+// Exercicio2: O prompt só retorna uma string, precisa converter para o
+// tipo numbre com number(). 
+// Exercicio3:ficando
+// let primeiroNumero = Number(prompt("Digite um numero!"))
+// let segundoNumero = Number(prompt("Digite outro numero!"))
+
+// const soma = primeiroNumero + segundoNumero
+
+// console.log(soma) 
+
+
+
+// // Exercicios de Escrita de código
+let idade = Number(prompt("Qual sua idade?"))
+let idadeDMelhorAmig = Number(prompt ("Qual a idade do seu melhor amigo(a)?"))
+
+console.log ("Sua idade é maior do que a do seu melhor amigo?", idade > idadeDMelhorAmig)
+const resultado = idade - idadeDMelhorAmig
+console.log(resultado)
+
