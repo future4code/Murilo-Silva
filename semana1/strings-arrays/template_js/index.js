1a21eb6f5130f1ae86f4c9968850a94e34f60490
Muri-lo <muriloss2019@hotmail.com>
2021-06-16 16:53:27 -0300

    Tarefa quase concluida

---
+++ semana1/strings-arrays/template_js/index.js
@@ -0,0 +1,43 @@
+//Exercicios de leitura de código:
+
+//Exercicio 1: 
+//Eu estava entendendo até a "c", na primeira o array ficou indefinido, na segunda foi nulificado, na terceira (c, respectivamente)
+//nidentifica quantos indices existem na array. Após esta parte fiquei completamente perdido ;-;
+
+
+//Exerciccio 2:
+ //Irá pedir para a pessoa colocar uma frase, depois ue ela colocar
+ //irá deixar tudo maiúsculo, trocar todas as letras "a" por "i"
+ //e contar quantos caracteres, letras e espaços tem no total.
+
+
+//Exercicios de excrita de código:
+//1)-
+// const nome = prompt('Digite seu nome:')
+// const email = prompt('Digite se E-mail:')
+// const mensagem = `O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}`
+
+// console.log(mensagem)
+
+
+
+//2)-feito 
+
+
+
+// const comidas = ['macarrão', 'nhoque', 'churrasco', 'pizza', 'esfiha']
+
+// const comidaOutro = prompt('Qual a sua comida favorita?')
+
+// comidas[1] = comidaOutro
+// console.log(comidas)
+
+
+//3)-
+// const listaDeTarefas = []
+// const tarefa1 = prompt('Qual sua primeira tarefa do dia?')
+// const tarefa2 = prompt('Qual sua segunda tarefa?')
+// const tarefa3 = prompt('Qual sua terceira tarefa do dia?')
+
+// listaDeTarefas.push(tarefa1, tarefa2, tarefa3)
+// console.log(listaDeTarefas)
