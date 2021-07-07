9c2a970b292a98920d47ab34957777e16b4eb7a2
Muri-lo <muriloss2019@hotmail.com>
2021-07-07 17:03:22 -0300

    Tive problemas como git, o conteúdo de uma branch não aparecia em outra, e como era trabakho no conteúdo feito ontem, surtei aqui, mas agora está resolvido

--- semana2/funcoes-de-array/index.js
+++ semana2/funcoes-de-array/index.js
@@ -57,18 +57,18 @@
 
 // 2)-
 
-const produtos = [
-    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
-    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
-    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
-    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
-    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
-    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
-    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
-    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
-    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
-    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
- ]
+// const produtos = [
+//     { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+//     { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+//     { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+//     { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+//     { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+//     { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+//     { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+//     { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+//     { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+//     { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+//  ]
 
 // A)-
 
