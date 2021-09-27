57938495f6c7a582c081dd47cb7270257612c186
Muri-lo <muriloss2019@hotmail.com>
2021-09-27 18:57:00 -0300

    projeto básico terminado, mas ainda é necessário corrigir alguns errinhos como o de error.message, que não soube o que fazer

---
+++ semana16/projeto-sistemaBancario/src/types.ts
@@ -0,0 +1,14 @@
+export type Transaction = {
+    value: number,
+    date: Date,
+    description: string,
+}
+
+export type Account = {
+    name: string,
+    CPF: string,
+    dateOfBirth: Date,
+    balance: number,
+    statement: Array<Transaction>
+}
+
