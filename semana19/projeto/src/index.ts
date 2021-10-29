a9943efae3cb27ec81fa0f7cb1ab44e7abe89634
Muri-lo <muriloss2019@hotmail.com>
2021-10-29 09:31:56 -0300

    Conexão com banco de dados estabelecida

--- semana19/projeto/src/index.ts
+++ semana19/projeto/src/index.ts
@@ -1,8 +1,11 @@
 import app from './app'
+import { connection } from './connection'
 import generateId from './services/idGenerator'
 
 app.get('/')
 
-console.log(
-    generateId()
-)
+// console.log(
+//     generateId()
+// )
+
+connection.raw("SHOW TABLES").then(console.log)
